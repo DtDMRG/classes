3d79a395ea25ca76e233d1ff7a64da1b19cc104b
Sam <samgenway@gmail.com>
2013-12-04 16:03:03 +0000

    simple test using matrix classes and template functions

--- source/matarray.cpp
+++ source/matarray.cpp
@@ -4,22 +4,51 @@
  * and the SVD routines provided.
  */
 
+/* Update: so far added is the use of the Eigen package for
+ * non-square matrices and an example of how to use template
+ * functions such that arguments and returns can be generic
+ * matrix objects.
+ */
+
 #include "Eigen/Dense"
 #include <iostream>
 using namespace Eigen;
 
-int main()
+#define complex std::complex<double>
+
+/* Function to multiply two matrices in canonical form
+ * for a given site with Nsig states and ni x nj submatrices
+ */
+
+//void canonmult(<matrixbase> &matrix){
+//}
+
+template <typename DerivedA,typename DerivedB>
+DerivedA squaredist(const MatrixBase<DerivedA>& p1,const MatrixBase<DerivedB>& p2)
 {
-  MatrixXd m(2,5);
-  m.resize(4,3);
-  std::cout << "The matrix m is of size "
-            << m.rows() << "x" << m.cols() << std::endl;
-  std::cout << "It has " << m.size() << " coefficients";
-  std::cout  << std::endl;
-  VectorXd v(2);
-  v.resize(5);
-  std::cout << "The vector v is of size " << v.size();
-  std::cout << std::endl;
-  std::cout << "As a matrix, v is of size "
-            << v.rows() << "x" << v.cols() << std::endl;
+  return p1*p2.transpose();
+}
+
+int main(void) {
+
+	int Nsigma = 3;		//Quantum dimension of a site
+	int Ni = 2;			//Dimension for matrix indices i
+	int Nj = 2;			//and j
+	int Nsigmai = Ni * Nsigma; //Canonical i dimension
+	typedef complex typeword;  //type for all matrices/arrays
+
+	Matrix<typeword, Dynamic, Dynamic> M(Nsigmai, Nj);
+	M = Matrix<typeword, Dynamic, Dynamic>::Random(Nsigmai, Nj);
+	std::cout << M << std::endl;
+	std::cout << std::endl;
+
+	std::cout << squaredist(M,2*M) << std::endl;
+
+	std::cout << std::endl;
+
+	Matrix<typeword, Dynamic, Dynamic> N = M.transpose();
+	std::cout << squaredist(N,N) << std::endl;
+
+
+
 }
