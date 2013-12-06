a016d0bd8972cf0e4cbf803e8892519e55391b54
Michael Hush <MichaelRHush@gmail.com>
2013-12-06 16:37:47 +0000

    Breaking local copy

--- source/mps.h
+++ source/mps.h
@@ -33,7 +33,6 @@ class Mps {
 	int n_sites, hilbert_dim;
 
 
-
 	//private functions
 	void sweep_from_Left_at(int); //Perform an SVD and then change the matrix at site and also pass the residue to site plus 1
 	void sweep_from_right_at(int); //*
@@ -44,6 +43,7 @@ public:
 
 	int stored_matrix_dimensions[];
 	MatrixXc stored_mps[];
+
 
 	//Constructor, Destructor
 	Mps(int, int);
