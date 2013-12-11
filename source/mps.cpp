ebbd7d6dc4c71472f32c0215010346534ce92f0a
emanuelelevi <emanuelelevi@lppjpg.ad.nottingham.ac.uk>
2013-12-11 09:33:39 +0000

    Another...

--- source/mps.cpp
+++ source/mps.cpp
@@ -7,7 +7,8 @@
 
 #include "mps.h"
 #define complex std::complex<double>
-
+#define cout std::cout
+#define endl std::endl
 
 
 /*CONSTRUCTOR
@@ -22,25 +23,25 @@ Mps::Mps(int ns, int db) {
 	n_sites = ns;
 	hilbert_dim = db;
 
-    //Define the column matrix of the down state
-	CanonMat M(db,1);
-    for (int i=0;i<db-1;i++){M(i,1)=complex(0.,0.);}
-    M(db-1,1)=1.0;
 
 
     //Resize the arrays to the right size
     stored_matrix_dimensions= new int[ns+1];
-    stored_mps = new CanonMat[ns];
+    mps_pointers = new const CanonMat*[ns];
 
+    //Define the column matrix of the down state
+
+    init= CanonMat( db ,1 );
+    for (int i=0;i<db-1;i++){init(i,0)=complex(double(i),0.);}
+    init(db-1,0)=1.0;
 
 
     //Initialize the dimensions vector to one for each site
 	for (int i=0;i<ns;i++){
 		stored_matrix_dimensions[i]=1;
-		stored_mps[i]=M;
+		mps_pointers[i]=&init;
 	}
 	stored_matrix_dimensions[ns]=1;
-
 
 }
 
@@ -50,5 +51,25 @@ Mps::Mps(int ns, int db) {
 
 
 /*DESTRUCTOR*/
-Mps::~Mps() {}
+Mps::~Mps(){}
+
+
+
+
+
+
+
+
+
+void Mps::sweep_from_left_at(int position, const CanonMat**& pointers){
+	cout<<pointers[position]<<endl;
+	Svd localsweep(*pointers[position]);
+	pointers[position]= & localsweep.matrixU();
+	cout<<pointers[position]<<endl;
+	cout<<*pointers[position]<<endl;
+}
+
+
+
+
 
